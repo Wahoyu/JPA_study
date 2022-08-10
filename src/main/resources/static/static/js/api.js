edcc39af82d6ddf3eb5df41fa4f9e35e80ca0b43
王泓宇 <645702818@qq.com>
2022-08-10 14:22:53 +0800

    还原了pc独立项目。可以正常进行注册，登陆，获取信息，退出。不过还差注册后跳转。

--- src/main/resources/static/static/js/api.js
+++ src/main/resources/static/static/js/api.js
@@ -1,57 +1,59 @@
-//注册行为
-function askVerifyCode(){
-    get('http://localhost:8080/api/auth/verify-code',{
-        email: $("#input-email").val()
-    },function (data){
-        //如果注册成功，直接跳转到登陆页面
-        if(data.code === 200){
-            window.location = "login.html"
-        }else{
-            alert(data.reason)
-        }
-    })
-}
-
 //登录请求
-function login(){
-    post('http://localhost:8080/api/auth/login',{
+function login() {
+    post('http://localhost:8080/api/auth/login',  {
         username: $("#username").val(),
         password: $("#password").val()
-    }, function (data){
-        if(data.code === 200){
+    }, function (data) {
+        if (data.code === 200) {
             window.location = "index.html"
-        }else{
+        } else {
             alert(data.reason)
         }
     })
 }
 
+//发送验证码
+function askVerifyCode() {
+    get('http://localhost:8080/api/auth/verify-code', {
+        email: $("#input-email").val()
+    }, function (data) {
+        //如果注册成功，直接跳转到登陆页面
+        if (data.code === 200) {
+            alert(data.reason)
+        }
+    })
+}
+
+//
+
+
 //退出登录
-function logout(){
-    get('http://localhost:8080/api/auth/logout' , function (data){
-        if(data.code === 200){
-            window.location="login.html"
+function logout() {
+    get('http://localhost:8080/api/auth/logout',{}, function (data) {
+        if (data.code === 200) {
+            window.location = "login.html"
         }
     })
 }
 
 //登录获取用户信息
-function initUserInfo(){
-    get('http://localhost:8080/api/user/info', function (data){
-        if(data.code === 200){
+function initUserInfo() {
+    get('http://localhost:8080/api/user/info',{}, function (data) {
+        if (data.code === 200) {
             $("#profile-name").text(data.data.username)
-        }else{
+        } else {
             window.location = 'login.html'
         }
     })
 }
 
 //封装get和post请求方法
-function get(url, success){
+function get(url, data, success) {
     $.ajax({
         type: "get",
         url: url,
         async: true,
+        data: data,
         dataType: 'json',
         xhrFields: {
             withCredentials: true
@@ -60,7 +62,7 @@ function get(url, success){
     });
 }
 
-function post(url, data, success){
+function post(url, data, success) {
     $.ajax({
         type: "post",
         url: url,
@@ -73,3 +75,4 @@ function post(url, data, success){
         success: success
     });
 }
+
